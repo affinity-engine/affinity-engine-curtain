1abf047deac1728daef0d5772ad800089c2b3e8b
null null null <null@ember.theater>
2016-05-30 11:14:19 -0700

    install correct version of preloadjs

--- blueprints/ember-theater-curtain/index.js
+++ blueprints/ember-theater-curtain/index.js
@@ -2,10 +2,9 @@ module.exports = {
   description: 'Generates files for ember theater curtain',
 
   afterInstall: function() {
-    var _this = this;
     var packages = [{
       name: 'PreloadJS',
-      target: '0.6.1'
+      target: '0.6.2'
     }];
 
     return this.addBowerPackagesToProject(packages);
