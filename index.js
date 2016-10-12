a45d2c868656909f9d26a2ffcbf50602cc85b315
null null null <null@affinitygamescollective.org>
2016-10-12 09:12:20 -0700

    remove createjs req from index.js

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-
-function findRoot(current) {
-  var app;
-
-  do {
-    app = current.app || app;
-  } while (current.parent && current.parent.parent && (current = current.parent));
-
-  return app;
-}
-
 module.exports = {
-  name: 'affinity-engine-curtain',
-
-  treeForAddon: function() {
-    var app = findRoot(this);
-
-    app.import(path.join(app.bowerDirectory, 'PreloadJS/lib/preloadjs-0.6.2.min.js'));
-
-    return this._super.treeForAddon.apply(this, arguments);
-  }
+  name: 'affinity-engine-curtain'
 };
